b0ddc68c8750b00b448e0f9b05c12579eadd2cfa
zexceed12300 <zexceed12300@gmail.com>
2020-09-01 12:22:55 +0000

    update: separate configuration

--- build.py
+++ build.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python
 
-# COMPILER SETUP
-ARCH                = "arm64"                           # Device Architecture
-BUILD_OUTPUT        = "../out"                          # Compiling output
-CROSS_COMPILE       = "/bin/aarch64-linux-android-"     # PATH to gcc toolchains
-CLANG_TRIPLE        = ""                                # leave empty if not using clang
-CC                  = ""                                # leave empty if not using clang
-
-# BUILD KERNEL IMAGE
-KERNELSTRING        = "EternalX-HMP-r1.5-rosy"
-DEFCONFIG           = "rosy-doge_defconfig"             # Kernel Configuration
-THREADS_JOBS        = 4                                 # Number of CPU threads to using it for compile
-
-# BUILD KLIB
-INSTALL_MOD_PATH    = "../"                             # Path to install Kernel Library And Modules
-
-# CREATE FLASHABLE
-FLASHABLE_NAME      = "KERNEL-EternalX-HMP-r1.5-rosy"   # Flashable zip name
-
-
 import time
 import subprocess as subp
 from subprocess import check_call
 import os
 import sys
 import shutil as copy
+import configparser
+
+config = configparser.ConfigParser()
+config.read('build.cfg')
+compiler_cfg = config['COMPILER_CFG']
+build_cfg = config['BUILD_CFG']
+klib_cfg = config['BUILD_KLIB_CFG']
+flashable_cfg = config['FLASHABLE_CFG']
+
+ARCH = compiler_cfg['ARCH']
+BUILD_OUTPUT = compiler_cfg['BUILD_OUTPUT']
+CROSS_COMPILE = compiler_cfg['CROSS_COMPILE']
+CLANG_TRIPLE = compiler_cfg['CLANG_TRIPLE']
+CC = compiler_cfg['CC']
+
+KERNELSTRING = build_cfg['KERNELSTRING']
+DEFCONFIG = build_cfg['DEFCONFIG']
+THREADS_JOBS = build_cfg['THREADS_JOBS']
+
+INSTALL_MOD_PATH = klib_cfg['INSTALL_MOD_PATH']
+
+ZIPNAME = flashable_cfg['ZIPNAME']
 
 def build_image():
     time.sleep(10)
@@ -32,13 +35,10 @@ def build_image():
         start_time = time.time()
         check_call(['echo', '---------------', 'BUILD_DEFCONFIG', '---------------'], stdout=log, stderr=log, stdin=subp.PIPE)
         check_call(['make', 'ARCH=%s' %ARCH, 'CROSS_COMPILE=%s' %CROSS_COMPILE, 'O=%s' %BUILD_OUTPUT, '%s' %DEFCONFIG], cwd='kernel', stdout=log, stderr=log, stdin=subp.PIPE)
-        try:
-            check_call(['echo', '\n------------------', 'COMPILING', '------------------'], stdout=log, stderr=log, stdin=subp.PIPE)
-            check_call(['make', 'ARCH=%s' %ARCH, 'CROSS_COMPILE=%s' %CROSS_COMPILE, 'O=%s' %BUILD_OUTPUT, '-j%s' %THREADS_JOBS], cwd='kernel', stdout=log, stderr=log, stdin=subp.PIPE)
-            end_time = time.time()  
-        except KeyboardInterrupt:
-            print('[*] Build Failed!')
-        if os.path.exist("./out/arch/arm64/boot/Image.gz-dtb"):
+        check_call(['echo', '\n------------------', 'COMPILING', '------------------'], stdout=log, stderr=log, stdin=subp.PIPE)
+        check_call(['make', 'ARCH=%s' %ARCH, 'CROSS_COMPILE=%s' %CROSS_COMPILE, 'O=%s' %BUILD_OUTPUT, '-j%s' %THREADS_JOBS], cwd='kernel', stdout=log, stderr=log, stdin=subp.PIPE)
+        end_time = time.time()  
+        if os.path.exists("./out/arch/arm64/boot/Image.gz-dtb"):
             print('[*] Build Success!')
         else:
             print('[*] Build Failed!')
@@ -54,4 +54,6 @@ def create_zip():
             if ARCH == "arm64":
                 copy.copy2("./out/arch/arm64/boot/Image.gz-dtb", "./anykernel/")
             check_call(['echo', '\n--------------', 'CREATE_FLASHABLE', '---------------'], stdout=log, stderr=log, stdin=subp.PIPE)
-            check_call(['zip', '-r9', '%s.zip' %FLASHABLE_NAME, '*'], cwd='anykernel', stdout=log, stderr=log, stdin=subp.PIPE)
+            check_call(['zip', '-r9', '%s.zip' %ZIPNAME, '*'], cwd='anykernel', stdout=log, stderr=log, stdin=subp.PIPE)
+
+build_image()
