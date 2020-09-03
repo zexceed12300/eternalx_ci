b56db666f421528464f744462864a86d970d6fc0
zexceed12300 <zexceed12300@gmail.com>
2020-09-03 22:20:30 +0000

    update: Telegram API & ArgumentParser & little changes

--- build.py
+++ build.py
@@ -37,9 +37,7 @@ ZIPNAME = "KERNEL-"+KERNELSTRING
 
 TELETOKEN = tele_notifier['TOKEN']
 
-def parameters():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("")
+date = subp.run(['date'], stdout=subp.PIPE).stdout.decode("utf-8")
 
 class TeleNotifier:
     def __init__(self):
@@ -50,14 +48,15 @@ class TeleNotifier:
         self.cfg = eval(str(data['result'][latest_id]['message']['text'].split()).replace("=", "':'").replace("[","{").replace("]", "}").replace("\s",""))
         self.chat_id = data['result'][latest_id]['message']['chat']['id']
 
-    def sendMessage(self, message):
+    def SendMessage(self, message):
         url = "https://api.telegram.org/bot"+TELETOKEN+"/sendMessage"
         data = {'chat_id': self.chat_id, 'parse_mode': 'html', 'text': message}
-        resp = requests.post(url, data=)
+        resp = requests.post(url, data=data)
 
     def SendFile(self, file):
         url = "https://api.telegram.org/bot"+TELETOKEN+"/sendDocument"
-        resp = requests.post(url, files={'document': file}).json()
+        data = {'chat_id': self.chat_id}
+        resp = requests.post(url, data=data, files={'document': file}).json()
 
     def SetEnviron(self):
         global KERNELSTRING
@@ -73,6 +72,33 @@ class TeleNotifier:
         global HOST
         HOST = self.cfg['HOST']
 
+def parameters():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--build-image", help="Build Kernel Image.gz-dtb And Modules", action="store_true")
+    parser.add_argument("--build-klib", help="Export Kernel Libraries and Modules", action="store_true")
+    parser.add_argument("--create-flashable", help="Packing Kernel Image and modules. Ready for flash", action="store_true")
+    args = parser.parse_args()
+
+    if args.build_image:
+        TeleNotifier().SetEnviron()
+        TeleNotifier().SendMessage('<b>[ + ] BUILDING STARTED!</b>\nat <b>{}</b>\n<b>Kernel String</b> : {}\n<b>Compiler</b> : {}\n<b>Defconfig</b> : {}\n<b>CPU Jobs</b> : {}\n<b>User</b> : {}\n<b>Host</b> : {}\n\n-- CircleCI script by zexceed12300'.format(date ,KERNELSTRING, COMPILER, DEFCONFIG, THREADS_JOBS, USER, HOST))
+        build_image()
+    else:
+        print("--build-image is required!")
+        sys.exit
+    if args.build_klib:
+        if args.build_image:
+            build_klib()
+        else:
+            print("cant --build-klib without --build-image")
+            sys.exit()
+    if args.create_flashable:
+        if args.build-image:
+            build_klib()
+        else:
+            print("cant --create-flashable without --build-image")
+            sys.exit()
+
 def build_image():
     with open('./build.log', 'w+') as log:
         start_time = time.time()
@@ -85,14 +111,49 @@ def build_image():
             else:
                 check_call(['make', 'ARCH=%s' %ARCH, 'KBUILD_BUILD_USER=%s' %USER, 'KBUILD_BUILD_HOST=%s' %HOST, 'CROSS_COMPILE=%s' %CROSS_COMPILE, 'O=%s' %BUILD_OUTPUT, '%s' %DEFCONFIG], cwd='kernel', stdout=log, stderr=log, stdin=subp.PIPE)
                 check_call(['echo', '\n------------------', 'COMPILING', '------------------'], stdout=log, stderr=log, stdin=subp.PIPE)
-                check_call(['make', 'ARCH=%s' %ARCH, 'KBUILD_BUILD_USER=%s' %USER, 'KBUILD_BUILD_HOST=%s' %HOST, 'CROSS_COMPILE=%s' %CROSS_COMPILE, 'O=%s' %BUILD_OUTPUT, '-j%s' %THREADS_JOBS], cwd='kernel', stdout=log, stderr=log, stdin=subp.PIPE)
-            end_time = time.time()  
+                check_call(['make', 'ARCH=%s' %ARCH, 'KBUILD_BUILD_USER=%s' %USER, 'KBUILD_BUILD_HOST=%s' %HOST, 'CROSS_COMPILE=%s' %CROSS_COMPILE, 'O=%s' %BUILD_OUTPUT, '-j%s' %THREADS_JOBS], cwd='kernel', stdout=log, stderr=log, stdin=subp.PIPE) 
+            elapsed_time = float(int(time.time() - start_time) / 60)
+            error_detail = ""
+            error_count = 0
+            warning_count = 0
             if os.path.exists("./out/arch/arm64/boot/Image.gz-dtb"):
                 print('KERNEL: Build Success!')
+                for i in open("build.log", "r").readlines():
+                    if "make" in i:
+                        i = "" 
+                    if "warning" in i:
+                        i = ""  
+                        warning_count += 1
+                    if ":" in i:
+                        if not error_detail:
+                            error_detail += i
+                            print(error_detail)
+                        error_count += 1
+                BuildFinish_message = '<b>[ * ] BUILDING FINISHED!</b>\nat <b>{}</b>\n<b>Elapsed Time</b> : {} min\n<b>Warning : {}</b>\n<b>Errors</b> : {}\n\n-- CircleCI script by zexceed12300'.format(date ,elapsed_time, warning_count, error_count)
+                TeleNotifier().SendMessage(BuildFinish_message)
+                TeleNotifier().SendFile(open("build.log", "rb"))
             else:
                 print('KERNEL: Build Failed!')
         except subp.CalledProcessError:
+                elapsed_time = float(int(time.time() - start_time) / 60)
+                error_detail = ""
+                error_count = 0
+                warning_count = 0
                 print('KERNEL: Build Failed!')
+                for i in open("build.log", "r").readlines():
+                    if "make" in i:
+                        i = "" 
+                    if "warning" in i:
+                        i = ""  
+                        warning_count += 1
+                    if ":" in i:
+                        if not error_detail:
+                            error_detail += i
+                            print(error_detail)
+                        error_count += 1
+                BuildFailed_message = '<b>[ ! ] BUILDING FAILED!</b>\nat <b>{}</b>\n<b>Error Preview: </b>\n{}.  .  .\n\n<b>Elapsed Time</b> : {:.2f} min\n<b>Warnings : {}</b>\n<b>Errors</b> : {}\n\n-- CircleCI script by zexceed12300'.format(date , error_detail, elapsed_time, warning_count, error_count)
+                TeleNotifier().SendMessage(BuildFailed_message)
+                TeleNotifier().SendFile(open("build.log", "rb"))
 
 def build_klib():
     with open('./build.log', 'r+') as log:
@@ -121,4 +182,5 @@ def create_zip():
                     print("ANYKERNEL: Build Failed!")
             except subp.CalledProcessError:
                 print("ANYKERNEL: Build Failed!")
-    
+
+parameters()
