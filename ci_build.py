48a496142b4693d415fc9330ce11437823716daa
XZEN <36181923+zexceed12300@users.noreply.github.com>
2020-09-17 13:08:35 +0700

    fix ZIPNAME not follow telegram environ

--- ci_build.py
+++ ci_build.py
@@ -99,6 +99,8 @@ class TeleNotifier:
         global HOST
         HOST = self.cfg['HOST']
         os.environ['KBUIL_BUILD_HOST'] = HOST
+        global ZIPNAME
+        ZIPNAME = "KERNEL"+KERNELSTRING
 
 def parameters():
     parser = argparse.ArgumentParser()
